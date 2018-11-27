de321f8648212ec4ab6d93b56f21474ed9d3f4b5
Marcin J <jerzakm@gmail.com>
2018-11-27 12:55:45 +0100

    range structures 1c

--- src/main/java/app/schemaBuilders/StandardBuilder.java
+++ src/main/java/app/schemaBuilders/StandardBuilder.java
@@ -13,6 +13,10 @@ public class StandardBuilder {
         makePointInTimeInt();
         makePointInTimeBigInt();
         makePointInTimeString();
+        makeRangeInTimeMoney();
+        makeRangeInTimeInt();
+        makeRangeInTimeBigInt();
+        makeRangeInTimeString();
     }
 
     private static void makePointInTimeMoney() {
@@ -61,7 +65,7 @@ public class StandardBuilder {
     }
 
     private static void makePointInTimeString() {
-        System.out.println("MAKING POINT IN TIME BIGINT DB - "+DbStructure.pointInTimeString.length + " tables defined");
+        System.out.println("MAKING POINT IN TIME STRING DB - "+DbStructure.pointInTimeString.length + " tables defined");
         Arrays.stream(DbStructure.pointInTimeString)
                 .forEach(tableName -> {
                     System.out.println("----- current table: "+tableName);
@@ -71,6 +75,70 @@ public class StandardBuilder {
                                     "value          TEXT        NOT NULL,\n" +
                                     "timestamp          BIGINT     NOT NULL,\n" +
                                     "PRIMARY KEY(offer_id, timestamp))";
+                    DbHandler.executeStatement(statement);
+                });
+    }
+
+    private static void makeRangeInTimeMoney() {
+        System.out.println("MAKING RANGE IN TIME MONEY DB - "+DbStructure.rangeInTimeMoney.length + " tables defined");
+        Arrays.stream(DbStructure.rangeInTimeMoney)
+                .forEach(tableName -> {
+                    System.out.println("----- current table: "+tableName);
+                    String statement =
+                            "CREATE TABLE "+tableName.toUpperCase()+"(\n" +
+                            "offer_id       INT        NOT NULL,\n" +
+                            "value          MONEY        NOT NULL,\n" +
+                            "start_time          BIGINT     NOT NULL,\n" +
+                            "end_time          BIGINT,\n" +
+                            "PRIMARY KEY(offer_id, start_time))";
+                    DbHandler.executeStatement(statement);
+                });
+    }
+
+    private static void makeRangeInTimeInt() {
+        System.out.println("MAKING RANGE IN TIME INT DB - "+DbStructure.rangeInTimeInt.length + " tables defined");
+        Arrays.stream(DbStructure.rangeInTimeInt)
+                .forEach(tableName -> {
+                    System.out.println("----- current table: "+tableName);
+                    String statement =
+                            "CREATE TABLE "+tableName.toUpperCase()+"(\n" +
+                                    "offer_id       INT        NOT NULL,\n" +
+                                    "value          INT        NOT NULL,\n" +
+                                    "start_time          BIGINT     NOT NULL,\n" +
+                                    "end_time          BIGINT,\n" +
+                                    "PRIMARY KEY(offer_id, start_time))";
+                    DbHandler.executeStatement(statement);
+                });
+    }
+
+    private static void makeRangeInTimeBigInt() {
+        System.out.println("MAKING RANGE IN TIME BIGINT DB - "+DbStructure.rangeInTimeBigInt.length + " tables defined");
+        Arrays.stream(DbStructure.rangeInTimeBigInt)
+                .forEach(tableName -> {
+                    System.out.println("----- current table: "+tableName);
+                    String statement =
+                            "CREATE TABLE "+tableName.toUpperCase()+"(\n" +
+                                    "offer_id       INT        NOT NULL,\n" +
+                                    "value          BIGINT        NOT NULL,\n" +
+                                    "start_time          BIGINT     NOT NULL,\n" +
+                                    "end_time          BIGINT,\n" +
+                                    "PRIMARY KEY(offer_id, start_time))";
+                    DbHandler.executeStatement(statement);
+                });
+    }
+
+    private static void makeRangeInTimeString() {
+        System.out.println("MAKING RANGE IN TIME STRING DB - "+DbStructure.rangeInTimeString.length + " tables defined");
+        Arrays.stream(DbStructure.rangeInTimeString)
+                .forEach(tableName -> {
+                    System.out.println("----- current table: "+tableName);
+                    String statement =
+                            "CREATE TABLE "+tableName.toUpperCase()+"(\n" +
+                                    "offer_id       INT        NOT NULL,\n" +
+                                    "value          TEXT        NOT NULL,\n" +
+                                    "start_time          BIGINT     NOT NULL,\n" +
+                                    "end_time          BIGINT,\n" +
+                                    "PRIMARY KEY(offer_id, start_time))";
                     DbHandler.executeStatement(statement);
                 });
     }
